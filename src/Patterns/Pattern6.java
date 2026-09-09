45c1067b81d8436bb93591f14f6f120c7a4ba261
Raj Aryan <rajaryansubham52@gmail.com>
2026-09-09 23:49:45 +0530

    Improve pattern 6 structure and documentation

--- src/Patterns/Pattern6.java
+++ src/Patterns/Pattern6.java
@@ -1,21 +1,43 @@
-package Day5;
-
 import java.util.Scanner;
 
 public class Pattern6
 {
     public static void main(String args[])
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter any no");
-        int n=sc.nextInt();
-        for(int i=0;i<n;i++)
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any no:");
+        int n = sc.nextInt();
+
+        /*
+            Pattern 6:
+            Print stars in decreasing order in each row.
+
+            Example for n = 5:
+
+            *****
+            ****
+            ***
+            **
+            *
+
+            Logic:
+            - Outer loop controls the rows.
+            - Inner loop prints the stars.
+            - Number of stars starts from n.
+            - One star is removed from each subsequent row.
+        */
+
+        for(int i = 0; i < n; i++)
         {
-            System.out.println("");
-            for(int j=n-i;j>0;j--)
+            for(int j = n - i; j > 0; j--)
             {
                 System.out.print("*");
             }
+
+            System.out.println();
         }
+
+        sc.close();
     }
 }
