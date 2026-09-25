3a49a039973699683a6b1ef53b3994be54941a54
gentleG1ant <rajaryansubham52@gmail.com>
2026-09-25 23:41:13 +0530

    Add Roman numeral to integer conversion

--- src/Strings/RomanToInteger.java
+++ src/Strings/RomanToInteger.java
@@ -12,7 +12,7 @@ class Roman {
         map.put('V', 5);
         map.put('X', 10);
         map.put('L', 50);
-        map.put('C', 100);
+        map.put('c', 100);
         map.put('D', 500);
         map.put('M', 1000);
 
